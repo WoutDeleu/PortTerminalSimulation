0197c3c729c42dd0157fc873aac627a52bcef325
Toon <eeraerts.toon@telenet.be>
2023-04-01 18:27:48 +0200

    samples

--- ContainerGroup.py
+++ ContainerGroup.py
@@ -1,11 +1,15 @@
 class ContainerGroup:
-    def __init__(self, type, number_of_containers, arrival_time, service_time, arrival_point, departure_point):
-        self.type = type
+    def __init__(self, container_type, number_of_containers, arrival_time, service_time, arrival_point, departure_point):
+        self.yard_block = None
+        self.container_type = container_type
         self.number_of_containers = number_of_containers
         self.arrival_time = arrival_time
         self.service_time = service_time
         self.arrival_point = arrival_point
         self.departure_point = departure_point
 
+    def add_yard_storage_block(self, yard_block):
+        self.yard_block = yard_block
+
     def __str__(self):
-        return f"{self.type}: {self.number_of_containers}, {self.arrival_time}, {self.service_time}, {self.arrival_point}, {self.departure_point}"
+        return f"{self.container_type}: {self.number_of_containers}, {self.arrival_time}, {self.service_time}, {self.arrival_point}, {self.departure_point}"
--- Simulation.py
+++ Simulation.py
@@ -3,7 +3,8 @@ from matplotlib import pyplot as plt
 
 from ContainerGroup import ContainerGroup
 from YardBlock import YardBlock
-from scipy.stats import expon
+import scipy.stats as scipyst
+import random
 import seaborn as sns
 
 
@@ -22,30 +23,30 @@ def simulate(data):
     time = 0  # Simulation clock
     container_groups = []  # List of all container groups in the yard block
     while time < period_in_hours:  # Stop the simulation after the given period
+        # Todo: find better sampling distributions (current ones are representative but probably not the best)
         time += get_inter_arrival_time_sample()
-        a = get_container_type_sample
-        b = get_number_of_containers_sample
-        c = get_service_time_sample
-        d = get_arrival_point_sample
-        e = get_departure_point_sample
+        a = get_container_type_sample()
+        b = get_number_of_containers_sample()
+        c = get_service_time_sample()
+        d = get_arrival_point_sample()
+        e = get_departure_point_sample()
         container_groups.append(ContainerGroup(a, b, time, c, d, e))  # New container arrived
 
 
 def get_inter_arrival_time_sample():
-    # Todo: find the right distribution (for now we use an exponential distribution but it's to steep)
-    return np.rint(expon.rvs(scale=3, loc=0))
+    return round(scipyst.expon.rvs(scale=3, loc=0))
 
 
 def get_container_type_sample():
-    return 0
+    return random.choices(["normal", "reefer"], weights=[69, 31], k=1)
 
 
 def get_number_of_containers_sample():
-    return 0
+    return round(scipyst.expon.rvs(scale=5, loc=0))
 
 
 def get_service_time_sample():
-    return 0
+    return round(random.uniform(0, 166))
 
 
 def get_arrival_point_sample():
