6238f5f95f6a140fc2e597a75f96beb82eef931e
WoutDeleu <wout.deleu@gmail.com>
2023-04-23 20:11:09 +0200

    Results

--- Main.py
+++ Main.py
@@ -60,18 +60,18 @@ def format_yb_table(data):
     for i in range(num_tables):
         start_index = i * 9
         end_index = min(start_index + 9, num_columns)
-        latex_table = '\\begin{table}[h]\n\\centering\n\\begin{tabular}{|' + '|'.join(
-            ['c'] * (end_index - start_index)) + '|}\n'
-        latex_table += '\\hline\n'
+        latex_table = "\\begin{table}[h]\n\\centering\n\\begin{tabular}{|" + "|".join(
+            ["c"] * (end_index - start_index)) + "|}\n"
+        latex_table += "\\hline\n"
         for j in range(start_index + 1, end_index + 1):
-            latex_table += 'YB' + str(j) + ' & '
-        latex_table = latex_table.rstrip('& ') + '\\\\\n'
-        latex_table += '\\hline\n'
+            latex_table += "YB" + str(j) + " & "
+        latex_table = latex_table.rstrip("& ") + "\\\\\n"
+        latex_table += "\\hline\n"
         for k in range(start_index, end_index):
-            latex_table += values[k].strip() + ' & '
-        latex_table = latex_table.rstrip('& ') + '\\\\\n'
-        latex_table += '\\hline\n'
-        latex_table += '\\end{tabular}\n\\caption{...}\n\\end{table}'
+            latex_table += values[k].strip() + " & "
+        latex_table = latex_table.rstrip("& ") + "\\\\\n"
+        latex_table += "\\hline\n"
+        latex_table += "\\end{tabular}\n\\caption{...}\n\\end{table}"
         latex_tables.append(latex_table)
     return latex_tables
 
@@ -87,13 +87,16 @@ def print_stats_latex(colnames, values):
         elif name == 'AVG_Daily_Individual_Occupancy':
             avg_daily_occ_latex = format_yb_table(value)
         else:
+            name = name.replace("_", " ")
             latex_table += f"{name} & {value} \\\\ \\hline\n"
     latex_table += "\\end{tabular}\n\\caption{...}\n\\end{table}"
 
     # Print de Latex-tabel
+    for s in avg_occ_latex:
+        print(s)
+    for s in avg_daily_occ_latex:
+        print(s)
     print(latex_table)
-    print(avg_occ_latex)
-    print(avg_daily_occ_latex)
 
 
 def load_data(folder):
