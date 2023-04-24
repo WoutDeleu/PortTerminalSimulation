994fc4d21dc4fe60c37b8232dc1e6b65e369d4f5
jef <jacobsjef16@gmail.com>
2023-04-24 11:20:10 +0200

    Gui draw squares

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+import math
 import tkinter
 import tkinter as tk
 
@@ -6,14 +7,21 @@ def start_simulation():
 
 def startGUI():
     gui = tk.Tk()
-    width = 500
-    height = 500
+    width = 1000
+    height = 1000
     gui.geometry(f"{width}x{height}")
 
     tk.Button(gui, text="Start", command=start_simulation).place(x=width - 65, y=height - 40)
     tk.Scale(gui,from_=0, to=200, orient=tk.HORIZONTAL).place(x=width/2 + 60, y=height - 55)
     w = tk.Canvas(gui, width=width, height=height - 200)
-    w.create_rectangle(0, 0, 100, 100, fill="blue", outline='blue')
+    # (X + 5) * n = Width
+    # (Y + 5) * m = Width
+    # n * m = total
+    for j in range(math.floor((height - 200) / 55)):
+        start_pos_y = j * (50 + 5)
+        for i in range(math.floor(width / 55)):
+            start_pos_x = i * (50 + 5)
+            w.create_rectangle(start_pos_x, start_pos_y, start_pos_x + 50, start_pos_y + 50, fill="black", outline='black')
     w.pack()
     gui.mainloop()
 
