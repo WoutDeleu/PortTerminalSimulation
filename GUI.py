bbb0937e7db102b1b554f6af30ed4625d33b4b23
jef <jacobsjef16@gmail.com>
2023-04-24 10:45:54 +0200

    Gui start

--- GUI.py
+++ GUI.py
@@ -1,9 +1,20 @@
-from tkinter import *
+import tkinter
+import tkinter as tk
+
+def start_simulation():
+    print("start sim")
 
 def startGUI():
-    root = Tk()
+    gui = tk.Tk()
+    width = 500
+    height = 500
+    gui.geometry(f"{width}x{height}")
 
-    label = Label(root, text="Hello World!")
+    tk.Button(gui, text="Start", command=start_simulation).place(x=width - 65, y=height - 40)
+    tk.Scale(gui,from_=0, to=200, orient=tk.HORIZONTAL).place(x=width/2 + 60, y=height - 55)
+    w = tk.Canvas(gui, width=width, height=height - 200)
+    w.create_rectangle(0, 0, 100, 100, fill="blue", outline='blue')
+    w.pack()
+    gui.mainloop()
 
-    label.grid(row=0, column=0)
-    root.mainloop()
+startGUI()
