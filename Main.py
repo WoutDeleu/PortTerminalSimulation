65fff63188fa3f81fc3cfd4ef2dcaf89e77197ea
jefjacobs00 <jacobsjef16@gmail.com>
2023-04-28 14:54:42 +0200

    changing the code to calculate the min runs required

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
+import math
 import os
 
+import matplotlib.pyplot as plt
 import pandas as pd
 from progress.bar import Bar
 
@@ -7,7 +9,7 @@ from Data.DataParser import parse_data
 from Result_Parser import show_result
 from Simulation import simulate_fifo
 
-AMOUNT_SIMULATIONS = 3
+AMOUNT_SIMULATIONS = 1000
 
 # LATEX formats table to copy paste in Latex-doc
 LATEX = False
@@ -38,18 +40,45 @@ def main():
                  'AVG_daily_total_Occupancy'])
 
     i = 1
+    d = 0.1
+    deviations = []
+    feature = 'AVG_Travel_Distance_Containers'
     # Progressbar - Only when using emulate in prompt
     with Bar('Simulating', fill='#', empty_fill='.', bar_prefix=' [',
              bar_suffix='] ', max=AMOUNT_SIMULATIONS) as bar:
         while i <= AMOUNT_SIMULATIONS:
             stats_fifo_closest_arrival = simulate_fifo(stats_fifo_closest_arrival, data, arrival_based=True)
             stats_fifo_closest_departure = simulate_fifo(stats_fifo_closest_departure, data, departure_based=True)
+            sample_variance = calculate_sample_variance(stats_fifo_closest_arrival, feature)
+            deviation = sample_variance/i
+            print('\n')
+            print(deviation)
+            deviations.append(deviation)
+            if deviation < d and i > 100:
+                break
             bar.next()
             i += 1
+    print(i)
     print('\n')
+
+    plt.plot(range(0, len(deviations)), deviations)
+    plt.ylim((0,round(max(deviations[1:]))))
+    plt.xlabel("Number of runs")
+    plt.ylabel(feature + " Deviation")
+    plt.title("Deviation based on " + feature)
+    plt.show()
     show_result('FIFO ARRIVAL-BASED', stats_fifo_closest_arrival, LATEX=LATEX, OVERVIEW=OVERVIEW)
     show_result('FIFO DEPARTURE-BASED', stats_fifo_closest_departure, LATEX=LATEX, OVERVIEW=OVERVIEW)
 
+
+def calculate_sample_variance(stats_fifo, feature):
+    average = stats_fifo[feature].mean()
+    sum = 0
+    for x in stats_fifo[feature]:
+        sum += math.pow(x - average, 2)
+    if len(stats_fifo[feature]) == 1:
+        return 99999999
+    return math.sqrt(sum / (len(stats_fifo[feature]) -1))
 
 if __name__ == '__main__':
     main()
