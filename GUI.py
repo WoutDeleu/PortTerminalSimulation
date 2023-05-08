aff1c151329efade9fa42efe301abd61b237678f
jefjacobs00 <jacobsjef16@gmail.com>
2023-05-08 19:39:01 +0200

    drawing the yardblocks

--- GUI.py
+++ GUI.py
@@ -7,21 +7,27 @@ def start_simulation():
 
 def startGUI():
     gui = tk.Tk()
-    width = 1000
-    height = 1000
+    rows = 10
+    cols = 16
+
+    yb_width = 80
+    yb_height = 40
+    tolerance = 20
+    yb = 159
+
+    width = cols * (yb_width + tolerance) + 200
+    height = rows * (yb_height + tolerance) + 500
     gui.geometry(f"{width}x{height}")
 
-    tk.Button(gui, text="Start", command=start_simulation).place(x=width - 65, y=height - 40)
-    tk.Scale(gui,from_=0, to=200, orient=tk.HORIZONTAL).place(x=width/2 + 60, y=height - 55)
-    w = tk.Canvas(gui, width=width, height=height - 200)
-    # (X + 5) * n = Width
-    # (Y + 5) * m = Width
-    # n * m = total
-    for j in range(math.floor((height - 200) / 55)):
-        start_pos_y = j * (50 + 5)
-        for i in range(math.floor(width / 55)):
-            start_pos_x = i * (50 + 5)
-            w.create_rectangle(start_pos_x, start_pos_y, start_pos_x + 50, start_pos_y + 50, fill="black", outline='black')
+    w = tk.Canvas(gui, width=width, height=height)
+
+    for i in range(yb):
+        row = math.floor(i / cols)
+        col = i - row * 16
+        start_pos_y = row * (yb_height + tolerance) + tolerance
+        start_pos_x = col * (yb_width + tolerance) + (200/2)
+        w.create_rectangle(start_pos_x, start_pos_y, start_pos_x + yb_width, start_pos_y + yb_height, fill="white", outline='orange')
+
     w.pack()
     gui.mainloop()
 
