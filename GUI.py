7f96c79ad18360b083e72f3021e347e8d4c7b953
jefjacobs00 <jacobsjef16@gmail.com>
2023-05-08 20:49:39 +0200

    update yardblock colors

--- GUI.py
+++ GUI.py
@@ -15,7 +15,7 @@ YB_HEIGHT = 40
 TOLERANCE = 20
 width = COLS * (YB_WIDTH + TOLERANCE) + 200
 height = ROWS * (YB_HEIGHT + TOLERANCE) + 500
-
+refresh_sec = 0.01
 
 def init_simulation():
     data = load_data('./Data/')
@@ -24,6 +24,7 @@ def init_simulation():
 
 
 def draw_yb(sim, canvas):
+    blocks = []
     for i in range(len(sim.yard_blocks)):
         row = math.floor(i / COLS)
         col = i - row * 16
@@ -33,15 +34,24 @@ def draw_yb(sim, canvas):
         yb_ocupancy = sim.yard_blocks[i].getOccupancy()
         fill = "#%02x%02x%02x" % (math.floor(255 * yb_ocupancy), math.floor(255 - 255 * yb_ocupancy), 0)
 
-        canvas.create_rectangle(start_pos_x,
+        rectangle = canvas.create_rectangle(start_pos_x,
                                 start_pos_y,
                                 start_pos_x + YB_WIDTH,
                                 start_pos_y + YB_HEIGHT,
                                 fill=fill,
                                 outline='orange')
+        blocks.append(rectangle)
 
     canvas.pack()
 
+    return blocks
+
+def update_ybs(gui, canvas, gui_blocks, yard_blocks):
+    for i in range(len(yard_blocks)):
+        yb_ocupancy = yard_blocks[i].getOccupancy()
+        fill = "#%02x%02x%02x" % (math.floor(255 * yb_ocupancy), math.floor(255 - 255 * yb_ocupancy), 0)
+        canvas.itemconfig(gui_blocks[i], fill=fill)
+    gui.update()
 
 def init_gui():
     gui = tk.Tk()
@@ -50,12 +60,12 @@ def init_gui():
     return gui
 
 
-def run_simulation(sim, canvas):
+def run_simulation(sim, gui, canvas):
     sim.setup_timers()
     departure_list = []
     arrival_list = [0]
 
-    draw_yb(sim, canvas)
+    blocks = draw_yb(sim, canvas)
     container_groups = []
     while sim.time < SIMULATION_HOURS:
         has_generated = sim.generate_new_time(departure_list, arrival_list)
@@ -71,13 +81,14 @@ def run_simulation(sim, canvas):
             sim.add_cg_to_closest_yb(new_cg, container_groups, departure_list)
             add_to_Q(arrival_list, sim.time + get_inter_arrival_time_sample())
             # update yb visualisation
+            update_ybs(gui, canvas, blocks, sim.yard_blocks)
 
 
 def startGUI():
     gui = init_gui()
     canvas = tk.Canvas(gui, width=width, height=height)
     sim = init_simulation()
-    run_simulation(sim, canvas)
+    run_simulation(sim, gui, canvas)
     gui.mainloop()
 
 
